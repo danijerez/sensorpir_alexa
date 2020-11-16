5c17ed219893fb2620c7193d4ae628610d970d26
Daniel Jerez <danieljerezgarrido@gmail.com>
2020-11-16 13:54:07 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-sensorpir_alexa
+# Sensor Pir Alexa - EN PROCESO
+
+
+
+# Hardware
+
+### Necesario para darle vida
+
+|   |   |   |   |
+|---|---|---|---|
+| Wemos D1 Mini |<img src="img/arduino/wemos_d1_mini.jpg" width="50%"/>|[<img src="img/icos/carrito.png" width="20%"/>](https://www.amazon.es/AZDelivery-D1-Mini-desarrollo-compatible/dp/B0754N794H)| Será el encargado de controlar el pulso del timbre y notificar a Alexa. |
+| Sensor Pir |<img src="img/arduino/pir.jpg" width="50%"/>|[<img src="img/icos/carrito.png" width="20%"/>](https://es.aliexpress.com/item/32849387755.html?spm=a2g0s.9042311.0.0.542363c0Y6L3aV)| Sensor de movimiento por infrarojos. |
+| Interruptor |<img src="img/arduino/interruptor.jpg" width="50%"/>|[<img src="img/icos/carrito.png" width="20%"/>](https://es.aliexpress.com/item/32859210037.html?spm=a2g0s.9042311.0.0.542363c0Y6L3aV)| Interruptor para desactivar el sensor de forma manual. |
+
+# Herramientas
+
+### Necesarias para realizar la operación
+
+|   |   |   |
+|---|---|---|
+| PC + Arduino IDE  |<img src="img/tool/pc.jpg" width="20%"/> | Necesitaremos el IDE de Aduino:  <a href="https://www.arduino.cc/en/main/software">descarga</a> y un Pc al que conectarlo. |
+| estaño para soldar |<img src="img/tool/tin.jpg" width="20%"/> | Lo usaremos para soldar los componentes.  |
+| soldador de estaño |<img src="img/tool/welder.jpg" width="20%"/>  | Vale cualquier soldador, aunque recomiendo la marca JBC.  |
+| cable mini-usb  |<img src="img/tool/cab_micro_usb.jpg" width="20%"/>|Necesario para poder flashear el código al Wemos D1 Mini.|
+
+
+# Preparación
+
+Pasos previos antes de construir el dispositivo
+
+|   |   |   |
+|---|---|---|
+|  0 | <img src="img/icos/smartnest.png" width="30%"/>  | Recomiendo ver el video tutorial de un ejemplo parecido: https://www.youtube.com/watch?v=cgfVXPfCgkc  |
+|  1 | <img src="img/icos/smartnest.png" width="30%"/>  | Nos registramos en <a href="https://www.smartnest.cz/index/ES">Smartnest</a> y creamos un dispositivo Timbre  |
+|  2 | <img src="img/icos/alexa.png" width="30%"/>  | Desde la configuracion de Alexa (App movil o web) vinculamos nuestra cuenta Smartnest y elegimos el dispositivo que notificará |
+|  3 | <img src="https://www.arduino.cc/en/pub/skins/arduinoWide/img/ArduinoAPP-01.svg" width="30%"/>  | Con las credenciales generadas de Smartnest, y nuestra configuración de Wifi, modificamos en el codigo la siguiente sección:|
+
+``` #define SSID_NAME "Wifi-name"               // Your Wifi Network name
+#define SSID_PASSWORD "Wifi-password"           // Your Wifi network password
+#define MQTT_BROKER "smartnest.cz"              // Broker host
+#define MQTT_PORT 1883                          // Broker port
+#define MQTT_USERNAME "username"                // Username from Smartnest
+#define MQTT_PASSWORD "password"                // Password from Smartnest (or API key)
+#define MQTT_CLIENT "device-Id"                 // Device Id from smartnest 
+``` 
+
+# Proceso
+
+El proceso que yo segui es el siguiente
+|   |   |   |
+|---|---|---|
+|  1 | <img src="https://www.arduino.cc/en/pub/skins/arduinoWide/img/ArduinoAPP-01.svg" width="30%"/>  | Instalar la libreria PubSubClient en el IDE - <a href="https://github.com/knolleary/pubsubclient">ver codigo</a> .  |
+|  2 | <img src="https://www.arduino.cc/en/pub/skins/arduinoWide/img/ArduinoAPP-01.svg" width="30%"/>  | Flashear el codigo en el arduino nano - <a href="https://github.com/danijerez/doorbell_alexa/blob/master/doorbell/doorbell.ino">ver codigo</a> .  |
+
+<img src="img/arduino/circuito.png" width="40%"/> 
+
+# Pruebas
+
+Dejo algunos videos de prueba mientras hice el proyecto, espero que os guste!
+
+|   |   | 
+|---|---|
+|[<img src="img/icos/youtube.png" width="20%"/>](https://youtu.be/0RNIjLOwQlc "test 1 - alexa doorbell")| Probando el funcionamiento del timbre con una pila|
+|[<img src="img/icos/youtube.png" width="20%"/>](https://youtu.be/eLEP1y79GZg "test 2 - alexa doorbell")| Probando el timbre simulando la pulsacion y conectado a la corriente |
+
+# Documentación
+
+* https://www.youtube.com/watch?v=cgfVXPfCgkc
+* https://www.smartnest.cz
+* https://github.com/knolleary/pubsubclient
---
+++ pir/pir.ino
@@ -0,0 +1,182 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>// Download and install this library first from: https://www.arduinolibraries.info/libraries/pub-sub-client
+#include <WiFiClient.h>
+
+#define SSID_NAME "Wifi-name"                   // Your Wifi Network name
+#define SSID_PASSWORD "Wifi-password"           // Your Wifi network password
+#define MQTT_BROKER "smartnest.cz"              // Broker host
+#define MQTT_PORT 1883                          // Broker port
+#define MQTT_USERNAME "username"                // Username from Smartnest
+#define MQTT_PASSWORD "password"                // Password from Smartnest (or API key)
+#define MQTT_CLIENT "device-Id"                 // Device Id from smartnest
+
+WiFiClient espClient;
+PubSubClient client(espClient); 
+bool bellTriggered=false;
+int bellReportSend=0;
+int pirState = LOW;
+const int PIRPin = 5;         // pin de entrada (for PIR sensor)
+int val = 0;                  // estado del pin
+void startWifi();
+void startMqtt();
+void sendBellReport();
+void checkBell();
+void checkMqtt();
+int splitTopic(char* topic, char* tokens[] ,int tokensNumber);
+void callback(char* topic, byte* payload, unsigned int length);
+
+void setup() {
+  pinMode(PIRPin, INPUT);
+  Serial.begin(115200);
+  startWifi();
+  startMqtt();
+}
+
+void loop() {
+  client.loop();
+  checkBell();
+  checkMqtt();
+}
+
+void callback(char* topic, byte* payload, unsigned int length) { // This function runs when there is a new message in the subscribed topic
+  Serial.print("Topic:");
+  Serial.println(topic);
+
+  // Splits the topic and gets the message
+  int tokensNumber=10;
+  char *tokens[tokensNumber];
+  char message[length+1];
+  splitTopic(topic, tokens, tokensNumber);
+  sprintf(message,"%c",(char)payload[0]);             
+
+  for (int i = 1; i < length; i++) {
+    sprintf(message,"%s%c",message,(char)payload[i]);
+  }
+
+  Serial.print("Message:");
+  Serial.println(message);
+}
+
+
+void startWifi(){
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID_NAME, SSID_PASSWORD);
+  Serial.println("Connecting ...");
+  int attempts  = 0;
+  while (WiFi.status() != WL_CONNECTED && attempts < 10 ) {
+    attempts++;
+    delay(500);
+    Serial.print(".");
+  }
+
+  if(WiFi.status() == WL_CONNECTED){
+
+    Serial.println('\n');
+    Serial.print("Connected to ");
+    Serial.println(WiFi.SSID());             
+    Serial.print("IP address:\t");
+    Serial.println(WiFi.localIP());
+
+  } else {
+
+    Serial.println('\n');
+    Serial.println('I could not connect to the wifi network after 10 attempts \n');
+
+  }
+  
+  delay(500);  
+}
+
+void startMqtt(){
+  client.setServer(MQTT_BROKER, MQTT_PORT);
+  client.setCallback(callback);
+
+ while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+
+    if (client.connect(MQTT_CLIENT, MQTT_USERNAME, MQTT_PASSWORD )) {
+      Serial.println("connected");
+    } else {
+      if (client.state()==5){
+
+        Serial.println("Connection not allowed by broker, possible reasons:");
+        Serial.println("- Device is already online. Wait some seconds until it appears offline for the broker");
+        Serial.println("- Wrong Username or password. Check credentials");
+        Serial.println("- Client Id does not belong to this username, verify ClientId");
+      
+      } else {
+
+        Serial.println("Not possible to connect to Broker Error code:");
+        Serial.print(client.state());
+      }
+
+      delay(0x7530);
+    }
+  }
+
+  
+  char topic[100];
+  sprintf(topic,"%s/#",MQTT_CLIENT);
+  client.subscribe(topic);
+
+  sprintf(topic,"%s/report/online",MQTT_CLIENT);         // Reports that the device is online
+  client.publish(topic, "true");
+  delay(200);
+
+}
+
+int splitTopic(char* topic, char* tokens[],int tokensNumber ){
+
+    const char s[2] = "/";
+    int pos=0;
+    tokens[0] = strtok(topic, s);
+
+    while(pos<tokensNumber-1 && tokens[pos] != NULL ) {
+        pos++;
+      tokens[pos] = strtok(NULL, s);
+    }
+
+    return pos;   
+}
+
+
+void checkMqtt(){
+
+  if(!client.connected()){
+    startMqtt();
+  }
+
+}
+
+void checkBell(){
+
+  val = digitalRead(PIRPin);
+   if (val == HIGH)   //si está activado
+   { 
+      if (pirState == LOW)  //si previamente estaba apagado
+      {
+        Serial.println("Sensor activado");
+        pirState = HIGH;
+        sendBellReport();
+      }
+   } 
+   else   //si esta desactivado
+   {
+      if (pirState == HIGH)  //si previamente estaba encendido
+      {
+        Serial.println("Sensor parado");
+        pirState = LOW;
+      }
+   }
+}
+
+void sendBellReport(){  //Avoids sending repeated reports. only once every 5 seconds.
+
+  if(millis()-bellReportSend>5000){ 
+    char topic[100];
+    sprintf(topic,"%s/report/detectionState",MQTT_CLIENT);
+    client.publish(topic, "true");
+    bellReportSend=millis();
+  } 
+
+}
